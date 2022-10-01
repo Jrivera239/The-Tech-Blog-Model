9df83620b9b6320d876b36913dceefd8e7ee8434
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-09-30 20:59:41 -0400

    getting server started to connect with Heroku

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebars');
+const expressHandlebars = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
+const SequelizeStore = require ("connect-session-sequelize")(session.Store);
+
+const section = {
+    secret: 'super secret secret', 
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore
+    ({db: sequelize})
+};
+app.use (session(section));
+const formatDate = require("utils/helpers");
+const handlebars = expressHandlebars.create({formatDate}
+);
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.use(express.json()
+);
+app.use(express.urlencoded({ extended: false })
+);
+app.use(express.static(path.join(__dirname, 'public'))
+);
+app.use(require('./controllers/')
+);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Hello and welcome!'));
+}
+);
