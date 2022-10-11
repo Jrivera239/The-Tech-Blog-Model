708487aab01cca18bcc0d0eaa7fe9ea278fed5a0
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-10-11 12:05:34 -0400

    debbuging handlebars

--- server.js
+++ server.js
@@ -1,46 +1,49 @@
-const express = require("express");
-const sequelize = require("./config/connection");
-const routes = require("./controllers");
-const session = require("express-session");
-const path = require("path");
-const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
-require("dotenv").config();
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-//cookie setup//
+// cookie setup //
 
 const sess = {
-  secret: process.env.DB_COOK,
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
+    secret: 'super secret secret', 
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
 };
 
 app.use(session(sess));
 
-// Express middleware
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
+
+
+
+//Allows us to use handlebars and Express middleware // 
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//Allows us to use handlebars //
-
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
 
 //Use all routes in controller folder //
 app.use(routes);
-
+app.use(require('./controllers/'));
 // Start server after DB connection //
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+  app.listen(PORT, () => console.log('Now listening'));
 });
