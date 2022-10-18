cef4516ad6231b4f4e861c90782768c6985f81be
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-10-18 19:00:34 -0400

    css

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
-const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const sess = {
     secret: "top secret as CIA and FBI",
-    cookie: {
-        maxAge: 20 * 90 * 1000
-    },
-    
+    cookie: {},
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
@@ -23,17 +18,19 @@ const sess = {
     })
 };
 
-app.use(session(sess));
+const helpers = require('./utils/helpers');
+
 const hbs = exphbs.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(routes);
 
-// connection to database //
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Connected'));
+    app.listen(PORT, () => console.log('Now listening'));
 });
