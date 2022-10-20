c52537fcd391365176e0a51d10fa183b9f48fd90
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-10-20 16:21:26 -0400

    renaming files for correction

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: '', 
+    secret: 'CIA type of secret', 
     cookie: {},
     resave: false,
     saveUninitialized: true,
@@ -18,15 +20,20 @@ const sess = {
 };
 
 app.use(session(sess));
+
 const helpers = require('./utils/helpers');
+
 const hbs = exphbs.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('And so it begins!'));
+    app.listen(PORT, () => console.log('Now listening'));
 });
